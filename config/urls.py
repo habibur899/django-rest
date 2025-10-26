6cef5476c087716c4a5789adb8746d7ea1a539e2
Habibur Rahaman <habiburg3@gmail.com>
2025-10-26 22:14:49 +0600

    Model

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from drapi import views
@@ -21,7 +5,6 @@ from drapi import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('aiinfo/', views.aiquest_info),
-    path('aiinfo/<int:pk>/', views.aiquest_info_single),
-    path('aicreate/', views.aiquest_create)
+    path('aicreate/', views.aiquest_create),
+    path('aicreate/<int:pk>/', views.aiquest_create)
 ]
--- drapi/views.py
+++ drapi/views.py
@@ -1,85 +1,26 @@
-import io
-
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Aiquest
 from .serializers import AiquestSerializer
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import random
+import string
 
 
-def aiquest_info(request):
-    # Complex data
-    ai = Aiquest.objects.all()
-    # Python dictionary
-    serializer = AiquestSerializer(ai, many=True)
-    # Render JSON
-    json_data = JSONRenderer().render(serializer.data)
-    # JSON sent to User
-    return HttpResponse(json_data, content_type='application/json')
-
-
-def aiquest_info_single(request, pk):
-    # Complex data
-    ai = Aiquest.objects.get(id=pk)
-    # Python dictionary
-    serializer = AiquestSerializer(ai)
-    # Render JSON
-    json_data = JSONRenderer().render(serializer.data)
-    # JSON sent to User
-    return HttpResponse(json_data, content_type='application/json')
-
-
-# Deserializers
-@csrf_exempt
-def aiquest_create(request):
-    if request.method == 'POST':
-        json_data = request.body
-        # JSON to Stream
-        stream = io.BytesIO(json_data)
-        # Stream to Python
-        pythondata = JSONParser().parse(stream)
-        # Python to complex
-        serializer = AiquestSerializer(data=pythondata)
-        if serializer.is_valid():
-            serializer.save()
-            res = {'msg': 'Successfully! insert data'}
-            json_data = JSONRenderer().render(res)
-            return HttpResponse(json_data, content_type='application/json')
-
-        json_data = JSONRenderer().render(serializer.errors)
-        return HttpResponse(json_data, content_type='application/json')
-
-    # Update data
-    if request.method == 'PUT':
-        json_data = request.body
-        # JSON to Stream
-        stream = io.BytesIO(json_data)
-        # Stream to Python
-        pythondata = JSONParser().parse(stream)
-        # Get object
-        ai = Aiquest.objects.get(id=pythondata['id'])
-        # Python to complex
-        serializer = AiquestSerializer(ai, data=pythondata, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            res = {'msg': 'Successfully! Updated data'}
-            json_data = JSONRenderer().render(res)
-            return HttpResponse(json_data, content_type='application/json')
-
-        json_data = JSONRenderer().render(serializer.errors)
-        return HttpResponse(json_data, content_type='application/json')
-
-    # Delete data
-    if request.method == 'DELETE':
-        json_data = request.body
-        # JSON to Stream
-        stream = io.BytesIO(json_data)
-        # Stream to Python
-        pythondata = JSONParser().parse(stream)
-        # Get object
-        ai = Aiquest.objects.get(id=pythondata['id'])
-        ai.delete()
-        res = {'msg': 'Successfully! Deleted data'}
-        json_data = JSONRenderer().render(res)
-        return HttpResponse(json_data, content_type='application/json')
+@api_view(['GET'])
+def aiquest_create(request, pk=None):
+    if request.method == 'GET':
+        id = pk
+        if id is not None:
+            # complex data
+            ai = Aiquest.objects.get(id=id)
+            # python dictionary
+            serializer = AiquestSerializer(ai)
+            return Response(serializer.data)
+        else:
+            # complex data
+            ai = Aiquest.objects.all()
+            # python dictionary
+            serializer = AiquestSerializer(ai, many=True)
+            return Response(serializer.data)
